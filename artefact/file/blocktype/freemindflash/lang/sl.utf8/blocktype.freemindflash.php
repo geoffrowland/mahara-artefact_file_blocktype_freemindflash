83abe33822cd823787add7e9d8e3ce530996bedb
Geoff Rowland <geoff.rowland@yeovil.ac.uk>
2011-05-18 09:50:38 +0100

    Actually add Slovenian translation from Gregor Anželj 	new file:   artefact/file/blocktype/freemindflash/lang/sl.utf8/blocktype.freemindflash.php

---
+++ artefact/file/blocktype/freemindflash/lang/sl.utf8/blocktype.freemindflash.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage artefact-freemindflash
+ * @author     James Kerrigan
+ * @author     Geoffrey Rowland 
+ * @translator     Gregor Anzelj, gregor.anzelj@gmail.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 James Kerrigan and Geoffrey Rowland geoff.rowland@yeovil.ac.uk
+ *
+ * Includes code from the FreeMind Flash Browser visorFreemind.swf 1.0b
+ * http://freemind.sourceforge.net/wiki/index.php/Flash_browser (GNU GPL licence)
+ *
+ * and expressInstall.swf and swfobject.js from SWFObject 2.2
+ * http://code.google.com/p/swfobject/ (MIT licence)
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'FreeMind Flash';
+$string['description'] = 'Prikažite datoteko (mm) z miselnim vzorcem';
+$string['showdescription'] = 'Prikažem opis?';
+$string['width'] = 'Širina';
+$string['height'] = 'Višina';
+$string['widthdescription'] = 'Določite širino (v pikslih). Pustite prazno, če želite uporabiti privzeto širino 450 px.';
+$string['heightdescription'] = 'Določite višino (v pikslih). Pustite prazno, če želite uporabiti privzeto višino 300 px.';
+?>
