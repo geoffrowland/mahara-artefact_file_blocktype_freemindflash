b1f0c637032dfa4dc71b473335fe8f55fe6b63eb
geoffrowland <geoff.rowland@yeovil.ac.uk>
2011-06-25 07:11:26 -0700

    Edited artefact/file/blocktype/freemindflash/lang/fr.utf8/blocktype.freemindflash.php via GitHub

--- artefact/file/blocktype/freemindflash/lang/fr.utf8/blocktype.freemindflash.php
+++ artefact/file/blocktype/freemindflash/lang/fr.utf8/blocktype.freemindflash.php
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    mahara
- * @subpackage artefact-cpds
+ * @subpackage blocktype-freemindflash
  * @author     James Kerrigan
  * @author     Geoffrey Rowland 
  * @author     Dominique-Alain JAN <djan@mac.com>
